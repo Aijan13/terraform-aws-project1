309a6d5385a59f6870e96a070d7783bfc006b0c4
Aijan13 <13muratova@gmail.com>
2024-04-24 01:34:36 +0000

    new files

--- module.tf
+++ module.tf
@@ -1,6 +1,4 @@
 
-
-```hcl
 module "s3_hosting_website" {
   source = "Aijan13/project1/aws"
   version = "0.0.5"
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable region {
+ type = string
+ description = "Provide region"
+}
+
+variable bucket_name {
+ type = string
+ description = "Provide S3 bucket name"
+}
+
+# variable subdomain_name {
+#  type = string
+#  description = "Provide subdomain name for Route53 record"
+# }
